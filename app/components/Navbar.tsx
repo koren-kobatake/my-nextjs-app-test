90ae51f14798c1d6600bd0d9ca6d06ccf1feef36
koren-kobatake <70135339+koren-kobatake@users.noreply.github.com>
2024-06-12 10:15:05 +0900

    Link修正

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,31 +2,32 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
+
     <nav className='bg-gray-800 p-4'>
       <ul className='flex space-x-4'>
         <li>
-          <Link href='/' legacyBehavior>
-            <a className='text-white'>Home</a>
+          <Link href='/' className='text-white'>
+            Home
           </Link>
         </li>
         <li>
-          <Link href='/about' legacyBehavior>
-            <a className='text-white'>About</a>
+          <Link href='/about' className='text-white'>
+            About
           </Link>
         </li>
         <li>
-          <Link href='/blog' legacyBehavior>
-            <a className='text-white'>Blog</a>
+          <Link href='/blog' className='text-white'>
+            Blog
           </Link>
         </li>
         <li>
-          <Link href='/baseball' legacyBehavior>
-            <a className='text-white'>Baseball</a>
+          <Link href='/baseball' className='text-white'>
+            Baseball
           </Link>
         </li>
         <li>
-          <Link href='/soccer' legacyBehavior>
-            <a className='text-white'>Soccer</a>
+          <Link href='/soccer' className='text-white'>
+            Soccer
           </Link>
         </li>
       </ul>
