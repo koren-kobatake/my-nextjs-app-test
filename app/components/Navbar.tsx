a5eb35c8dc74991a9e8b66b1d70bf4e6428418f0
koren-kobatake <70135339+koren-kobatake@users.noreply.github.com>
2024-06-12 10:31:03 +0900

    pages作成

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,22 +11,22 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <Link href='/about' className='text-white'>
+          <Link href='/pages/about' className='text-white'>
             About
           </Link>
         </li>
         <li>
-          <Link href='/blog' className='text-white'>
+          <Link href='/pages/blog' className='text-white'>
             Blog
           </Link>
         </li>
         <li>
-          <Link href='/baseball' className='text-white'>
+          <Link href='/pages/baseball' className='text-white'>
             Baseball
           </Link>
         </li>
         <li>
-          <Link href='/soccer' className='text-white'>
+          <Link href='/pages/soccer' className='text-white'>
             Soccer
           </Link>
         </li>
